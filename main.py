e9abaa9ba6803fbc026465b30514d081c190335d
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2021-08-26 02:45:01 +0530

    new style changes & code changes

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template, redirect, url_for
+import requests
+from requests.auth import HTTPBasicAuth
+from flask import Flask, render_template, redirect, url_for, send_from_directory
 
 app = Flask(__name__)
+
+github_credentials = {
+    'username': 'Nadunnissanka',
+    'password': 'Nadun@123'
+}
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    authentication = HTTPBasicAuth(github_credentials['username'], github_credentials['password'])
+    git_data = requests.get('https://api.github.com/users/' + github_credentials['username'], auth=authentication)
+    git_stats = git_data.json()
+    return render_template('index.html', github=git_stats)
+
+
+@app.route('/download')
+def download():
+    return send_from_directory('static', filename="files/service_letter_one.pdf", path="static")
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
